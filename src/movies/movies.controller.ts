231db9449ac1542c6fc0c78f6d8cdb3ee94b8be3
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-10-11 15:06:08 +0900

    Add:movie_controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+    @Get()
+    getAll() {
+        return 'This will return all movies';
+    }
+    
+    @Get('search')
+    search(@Query('year') searchingYear:string) {
+        return `we are searching for a movie made after ${searchingYear}`;
+    }
+
+    @Get(':id')
+    getOne(@Param('id') movieId: string) {
+        return `This will return one movie with the id: ${movieId}`
+    }
+
+    @Post()
+    create(@Body() movieData){
+        console.log(movieData);
+        return movieData;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') movieId:string){
+        return `This will delete a movie with the id : ${movieId}`;
+    }
+
+    @Patch(':id') 
+    path(@Param('id') movieId:string, @Body() updateData) {
+        return {
+            updateMovie :movieId,
+            ...updateData 
+        };
+    }
+
+}
